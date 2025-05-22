6af764e622daa7759475c2b7ad10f4df0fd39ab6
Louis Remes <louisremes@hotmail.be>
2025-05-22 21:24:44 +0200

    added split_nodes_delimiter

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -43,6 +43,46 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node!", TextType.TEXT, "www.boot.dev")
         node2 = TextNode("This is a text node?", TextType.BOLD, "www.boot.devs")
         self.assertNotEqual(node, node2)
+
+    def test_split_nodes_delimiter_1(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        node2 = [TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),]
+        self.assertEqual(split_nodes_delimiter([node], "`", TextType.CODE), node2)
+
+    def test_split_nodes_delimiter_2(self):
+        node = TextNode("This is text with a _code block_ word", TextType.TEXT)
+        node2 = [TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.ITALIC),
+                TextNode(" word", TextType.TEXT),]
+        self.assertEqual(split_nodes_delimiter([node], "_", TextType.ITALIC), node2)
+
+    def test_split_nodes_delimiter_3(self):
+        node = TextNode("This is text with a _code block_ word", TextType.TEXT)
+        node2 = [TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.LINK),
+                TextNode(" word", TextType.TEXT),]
+        self.assertEqual(split_nodes_delimiter([node], "_", TextType.LINK), node2)
+
+    def test_split_nodes_delimiter_4(self):
+        node = TextNode("`code block` word", TextType.TEXT)
+        node2 = [TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),]
+        self.assertEqual(split_nodes_delimiter([node], "`", TextType.CODE), node2)
+
+    def test_split_nodes_delimiter_5(self):
+        node = TextNode("`code block` word", TextType.TEXT)
+        node2 = [TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),]
+        node2.extend(node2)
+        self.assertEqual(split_nodes_delimiter([node, node], "`", TextType.CODE), node2)
+
+    def test_split_nodes_delimiter_6(self):
+        node = TextNode("__", TextType.TEXT)
+        node2 = []
+        self.assertEqual(split_nodes_delimiter([node, node], "__", TextType.CODE), node2)
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -21,3 +21,20 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            nodes.append(old_node)
+        elif delimiter not in old_node.text:
+            raise Exception("No delimiter in node")
+        else:
+            for iter, split_node in enumerate(old_node.text.split(delimiter)):
+                if split_node:
+                    if iter  % 2:
+                        nodes.append(TextNode(split_node, text_type))
+                    else:
+                        nodes.append(TextNode(split_node, old_node.text_type))
+    return nodes
+                
