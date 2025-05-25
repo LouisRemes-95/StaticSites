7b1a8acc9647ffda8a2386ed8512b8d1e69c3188
Louis Remes <louisremes@hotmail.be>
2025-05-25 20:35:23 +0200

    added build.sh

---
+++ build.sh
@@ -0,0 +1,1 @@
+python3 src/main.py "/LouisRemes-95/StaticSites/"
--- src/main.py
+++ src/main.py
@@ -1,14 +1,21 @@
+import sys
+from pathlib import Path
+
 from copy_directory_and_content import *
 from htmlnode import markdown_to_html_node
 from markdown_processing import extract_title
-from pathlib import Path
 
 
 def main():
-    copy_directory_and_content("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+
+    copy_directory_and_content("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
+
+
+
     
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as file:
         markdown = file.read()
@@ -18,15 +25,17 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, 'w') as file:
         file.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if Path(dir_path_content).suffix == ".md":
-        generate_page(dir_path_content, template_path, Path(dest_dir_path).with_suffix(".html"))
+        generate_page(dir_path_content, template_path, Path(dest_dir_path).with_suffix(".html"), basepath)
     elif Path(dir_path_content).is_dir():
         for element in os.listdir(dir_path_content):
-            generate_pages_recursive(os.path.join(dir_path_content, element), template_path, os.path.join(dest_dir_path, element))
+            generate_pages_recursive(os.path.join(dir_path_content, element), template_path, os.path.join(dest_dir_path, element), basepath)
 
 main()
